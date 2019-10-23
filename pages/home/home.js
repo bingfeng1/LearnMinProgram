269b997d68f1504998dd6d3a50d6257a9bf906f9
冰风 <wuxiaoming@baosight.com>
2019-10-23 13:55:39 +0800

    生命周期、标题
    小程序生命周期、标题底部tabbar设置

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,45 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'world',
+    age: 19,
+    students: [{
+        id: 1,
+        name: 'aaaa',
+        age: 10
+      },
+      {
+        id: 2,
+        name: 'bbbb',
+        age: 20
+      },
+      {
+        id: 3,
+        name: 'ccccc',
+        age: 30
+      },
+      {
+        id: 4,
+        name: 'ddddd',
+        age: 40
+      },
+      {
+        id: 5,
+        name: 'eeee',
+        age: 50
+      },
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    let num = this.data.counter + 1
+    this.setData({
+      counter: num
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    let num = this.data.counter - 1
+    this.setData({
+      counter: num
+    })
   }
 })
