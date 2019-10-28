3b081c1c25d038c7cbaacd65c14309dd4a1d865e
冰风 <wuxiaoming@baosight.com>
2019-10-28 11:15:47 +0800

    0808_弹出层

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,41 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中',
+      duration:3000,
+      icon:'loading'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title: '标题',
+      content: '内容',
+      success(res){
+        console.log(res)
+      }
+    })
   },
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载中',
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
+    setTimeout(() => {
+      wx.hideLoading()
+    },1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['aaaa','ccccccc'],
+      success(res){
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage(){
+    
   }
 })
+
